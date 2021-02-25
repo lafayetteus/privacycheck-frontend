7ae3993a43a4083a74f2ef4c1db5f79bd8fdb18a
Lucas Vocos <lucasvocos@gmail.com>
2021-02-25 13:17:15 -0500

    clean up sanity.js util

--- src/api/sanity.js
+++ src/api/sanity.js
@@ -1,7 +1,7 @@
 const sanityClient = require("@sanity/client")
 
 module.exports = sanityClient({
-  projectId: process.env.GATSBY_SANITY_PROJECT_ID || "ut33d2oh",
+  projectId: process.env.GATSBY_SANITY_PROJECT_ID,
   dataset: "production",
   useCdn: false,
 })
